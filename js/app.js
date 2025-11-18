9a5e1bdac3b4586d6203668b9738a1d9631cb512
Oğulcem Yıldız <91990302+cemyyldz@users.noreply.github.com>
2025-11-19 00:16:51 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const seesaw=document.querySelector('.seesaw-wrapper');
 const seesawElement=document.querySelector('.seesaw');
+let weights=[];
 seesaw.addEventListener('click',(e) => {
   const rect=seesaw.getBoundingClientRect();
   const clickX =e.clientX - rect.left;
@@ -8,11 +9,37 @@ seesaw.addEventListener('click',(e) => {
   
   const weightDiv=document.createElement('div');
   weightDiv.classList.add('weight')
-  weightDiv.style.left=`$[clickX-10}px`;
+  weightDiv.style.left=`${clickX-10}px`;
   weightDiv.style.bottom='20px';
-  weightDiv.textContent=weightValue;
+  weightDiv.textContent=weightValue + "kg";
 
   seesaw.appendChild(weightDiv);
+  weights.push({x: clickX,weight:weightValue});
+  updateSeesaw();
+
+
 
   console.log(`Yeni ağırlık:${weightValue}kg,X:${clickX}px`);
 });
+
+function updateSeesaw(){
+  const seesawWidth=seesaw.offsetWidth;
+  const pivotX=seesawWidth / 2;
+
+  let leftTorque = 0;
+  let rightTorque = 0;
+
+  weights.forEach(obj => {
+    const distance = obj.x - pivotX;
+    if(distance<0){
+      leftTorque += obj.weight *Math.abs(distance)
+    } else {
+      rightTorque += obj.weight *distance;
+    }
+  });
+
+  let angle = (rightTorque - leftTorque)/100;
+  angle = Math.max(-30,Math.min(30,angle));
+
+  seesawElement.style.transform = `rotate(${angle}deg)`;
+}
